8e42ed810c5746673a553d7912b30173cdb0a4f9
Ross Hathaway <hathaway.ross@gmail.com>
2020-06-01 21:03:02 -0400

    dummy tests work with mocha

--- test/test.js
+++ test/test.js
@@ -1,4 +1,14 @@
 const assert = require('assert');
 const rollupStarterPlugin = require('..');
 
-assert.strictEqual(rollupStarterPlugin().name, 'starter-plugin');
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+
+    it('testing if tests are working', function () {
+      assert.strictEqual(rollupStarterPlugin().name, 'starter-plugin');
+    });
+  });
+});
