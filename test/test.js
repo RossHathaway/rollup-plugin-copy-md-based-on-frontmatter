1e731e22c696daeb98c4cbc3b9b6695114ca5f0b
Ross Hathaway <hathaway.ross@gmail.com>
2020-06-03 12:47:45 -0400

    test with node instead of mocha

--- test/test.js
+++ test/test.js
@@ -1,21 +1,21 @@
 const assert = require('assert');
 const rollupStarterPlugin = require('..');
-const five = require('../src/test-export');
+// const five = require('../src/test-export.js');
+// console.log('rollupStarterPlugin', rollupStarterPlugin);
+// describe('Array', function () {
+//   describe('#indexOf()', function () {
+// it('should return -1 when the value is not present', function () {
+assert.equal([1, 2, 3].indexOf(4), -1);
+// });
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+// it('testing if tests are working', function () {
+assert.strictEqual(rollupStarterPlugin().name, 'starter-plugin');
+// });
+// });
 
-    it('testing if tests are working', function () {
-      assert.strictEqual(rollupStarterPlugin().name, 'starter-plugin');
-    });
-  });
-
-  describe('test of import', function () {
-    it('should be able to import', function () {
-      assert.equal(five, 5);
-    });
-  });
-});
+// describe('test of import', function () {
+//   it('should be able to import', function () {
+// assert.equal(five, 5);
+//   });
+// });
+// });
