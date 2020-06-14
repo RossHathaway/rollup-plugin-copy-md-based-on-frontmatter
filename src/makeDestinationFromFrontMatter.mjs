88c2936540b441893a20a71a0633aeb8e8ab589f
Ross Hathaway <hathaway.ross@gmail.com>
2020-06-13 23:33:12 -0400

    add try catch back

--- src/makeDestinationFromFrontMatter.mjs
+++ src/makeDestinationFromFrontMatter.mjs
@@ -20,46 +20,46 @@ export default function makeDestinationFromFrontMatter(
     pathSection: 'folder_name',
   }
 ) {
-  // try {
-  const destinations = [];
-  let startIndex =
-    frontMatter.indexOf(`- ${names.parent}: \n`) + names.parent.length + 7;
-  const locationYaml = frontMatter.slice(startIndex)
-  const yamlSplitOnNewLine = locationYaml.split('\n')
-  let isFirstPathSection = true
+  try {
+    const destinations = [];
+    let startIndex =
+      frontMatter.indexOf(`- ${names.parent}: \n`) + names.parent.length + 7;
+    const locationYaml = frontMatter.slice(startIndex)
+    const yamlSplitOnNewLine = locationYaml.split('\n')
+    let isFirstPathSection = true
 
-  for (const line of yamlSplitOnNewLine) {
+    for (const line of yamlSplitOnNewLine) {
 
-    if (line.trimStart() === `- ${names.path}:`) {
-      destinations.push('')
-      isFirstPathSection = true
-    } else if (line.trimStart().startsWith(`- ${names.pathSection}: `)) {
-      const pathSection = line.trimStart().slice(names.pathSection.length + 4)
-      const indexToAddTo = destinations.length - 1
-      const pathSectionToAdd = isFirstPathSection ? pathSection : '/' + pathSection
-      isFirstPathSection = false
-      destinations[indexToAddTo] += pathSectionToAdd
+      if (line.trimStart() === `- ${names.path}:`) {
+        destinations.push('')
+        isFirstPathSection = true
+      } else if (line.trimStart().startsWith(`- ${names.pathSection}: `)) {
+        const pathSection = line.trimStart().slice(names.pathSection.length + 4)
+        const indexToAddTo = destinations.length - 1
+        const pathSectionToAdd = isFirstPathSection ? pathSection : '/' + pathSection
+        isFirstPathSection = false
+        destinations[indexToAddTo] += pathSectionToAdd
 
-    } else if (line === '\n') {
-      // continue to next line
-    } else break
+      } else if (line === '\n') {
+        // continue to next line
+      } else break
+    }
+
+    console.log('destinations', destinations)
+    return destinations;
+  } catch (e) {
+    throw new Error(
+      `Unable to get a destination path from the front matter ${frontMatter}
+       Check example front matter and make sure it matches the properties on the names pbject you passed in. If you use the default names object, the front matter would need to be formatted like this:
+
+      document_locations:
+      - location:
+        - folder_name: parent-folder
+        - folder_name: child-folder
+      - location:
+        - folder_name: parent-folder
+        - folder_name: child-folder
+        - folder_name: grandchild-folder`
+    );
   }
-
-  console.log('destinations', destinations)
-  return destinations;
-  // } catch (e) {
-  //   throw new Error(
-  //     `Unable to get a destination path from the front matter ${frontMatter}
-  //      Check example front matter and make sure it matches the properties on the names pbject you passed in. If you use the default names object, the front matter would need to be formatted like this:
-
-  //     document_locations:
-  //     - location:
-  //       - folder_name: parent-folder
-  //       - folder_name: child-folder
-  //     - location:
-  //       - folder_name: parent-folder
-  //       - folder_name: child-folder
-  //       - folder_name: grandchild-folder`
-  //   );
-  // }
 }
